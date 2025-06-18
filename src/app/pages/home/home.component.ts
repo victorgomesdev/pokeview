681aea8f2f419b75a4fca5fd94c3f2fff7e46b39
Victor <victorgomesnog123@gmail.com>
2025-06-18 17:36:14 -0300

    fix: Algumas correções.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,19 +1,21 @@
 import { Component, inject, OnInit } from "@angular/core";
 import { NgFor } from "@angular/common";
-import { IonHeader, IonTitle, IonToolbar, IonContent, IonCard, IonCardContent, IonCardTitle, IonCardHeader, IonImg } from '@ionic/angular/standalone'
+import { IonHeader, IonTitle, IonToolbar, IonContent, IonInfiniteScroll, IonInfiniteScrollContent, IonList, IonItem, IonAvatar, IonLabel, InfiniteScrollCustomEvent } from '@ionic/angular/standalone'
 import { PokemonService } from "src/app/services/pokemon.service";
 import { CapitalizeNamePipe } from "src/app/common/pipes/capitalize-name.pipe";
 
 @Component({
   templateUrl: './home.component.html',
   selector: 'home-component',
-  imports: [IonHeader, IonTitle, IonToolbar, IonContent, IonCard, NgFor, IonCardContent, IonCardTitle, IonCardHeader, CapitalizeNamePipe, IonImg]
+  imports: [IonHeader, IonTitle, IonToolbar, IonContent, NgFor, CapitalizeNamePipe, IonInfiniteScroll, IonInfiniteScrollContent, IonList, IonItem, IonAvatar, IonLabel]
 })
 export class HomePageComponent implements OnInit {
 
 
   filteredPokemons!: any[]
   pokemonsWithData!: any[]
+
+  limit = 100
 
   pokemonService = inject(PokemonService)
 
@@ -26,6 +28,23 @@ export class HomePageComponent implements OnInit {
             .subscribe({
               next: (res) => {
                 this.pokemonsWithData = res
+              }
+            })
+        }
+      })
+  }
+
+  onScrollEnd(scrollEvent: InfiniteScrollCustomEvent): void {
+    this.limit += 5
+    this.pokemonService.listAllPokemonsByFilter(undefined, this.limit)
+      .subscribe({
+        next: (res) => {
+          const loaded = res.results.map(p => p.name)
+          this.filteredPokemons.push(loaded)
+          this.pokemonService.getPokemonDataByName(loaded)
+            .subscribe({
+              next: (res) => {
+                this.pokemonsWithData.push(res)
               }
             })
         }
